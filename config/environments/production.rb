0965441fef4942fa5cdf28adbecbeb695025109c
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2022-10-27 11:48:17 -0300

    FInal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,14 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://arcane-oasis-05603.herokuapp.com/ " }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.mail.com',
     port:                 587,
     domain:               'gmail.com',
     user_name:            ENV['SMTP_USER_NAME'],
     password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain'
+    authentication:       'plain',
+    enable_starttls_auto: true
     }
   # Settings specified here will take precedence over those in config/application.rb.
 
