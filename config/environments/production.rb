9a32532207f9dbb73969cbae27e8dd875729ede5
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2022-10-27 01:55:14 -0300

    Alteracoes finais

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "" }
+  config.action_mailer.default_url_options = { host: "https://arcane-oasis-05603.herokuapp.com/ " }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
